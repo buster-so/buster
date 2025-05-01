043c5dd3ee92bda7a0b19b966412eff6a9345786
Nate Kelley <nate@buster.so>
2025-05-01 15:47:18 -0600

    smoke test for loading pages

--- web/playwright.config.ts
+++ web/playwright.config.ts
@@ -20,8 +20,8 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  /* Run 3 tests in parallel */
+  workers: process.env.CI ? 3 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
