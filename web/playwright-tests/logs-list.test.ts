1954015ea6b13ad533ce4469d493050de0c1245a
Nate Kelley <nate@buster.so>
2025-05-05 15:50:02 -0600

    add bar chart navigations

--- web/playwright-tests/logs-list.test.ts
+++ web/playwright-tests/logs-list.test.ts
@@ -5,7 +5,7 @@ const logsRoute = createBusterRoute({
   route: BusterRoutes.APP_LOGS
 });
 
-test.skip('Can navigate to a metric chart from the metric list', async ({ page }) => {
+test('Can navigate to a metric chart from the metric list', async ({ page }) => {
   await page.goto('http://localhost:3000/app/home');
   await page.getByRole('link', { name: 'Logs' }).click();
   await expect(page.getByText('Name')).toBeVisible();
