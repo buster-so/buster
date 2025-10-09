cff29fb307f7624f08a2f2931e31e62744de4d35
Nate Kelley <nate@buster.so>
2025-10-09 13:39:09 -0600

    Update SearchLoading.tsx

--- apps/web/src/components/ui/search/SearchModal/SearchLoading.tsx
+++ apps/web/src/components/ui/search/SearchModal/SearchLoading.tsx
@@ -10,7 +10,7 @@ export const SearchLoading = ({
   showTopLoading?: boolean;
 }) => {
   return (
-    <Command.Loading className="w-full border-b swag relative">
+    <Command.Loading className="w-full border-b relative">
       {showTopLoading && loading && (
         <IndeterminateLinearLoader
           className={cn('w-full absolute top-0 left-0 right-0')}
