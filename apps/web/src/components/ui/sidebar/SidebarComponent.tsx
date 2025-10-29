bcf99157236c3d39f2103c96ee49edcb5a0832a7
Nate Kelley <nate@buster.so>
2025-10-29 10:34:25 -0600

    scrollable sidebar

--- apps/web/src/components/ui/sidebar/SidebarComponent.tsx
+++ apps/web/src/components/ui/sidebar/SidebarComponent.tsx
@@ -9,17 +9,22 @@ export const Sidebar: React.FC<SidebarProps> = React.memo(
   ({ header, content, footer, useCollapsible = true, onCollapseClick }) => {
     return (
       <SidebarContextProvider onCollapseClick={onCollapseClick}>
-        <div className="@container flex h-full flex-col overflow-hidden px-3.5 pt-4.5">
-          <div className="flex flex-col space-y-4.5">
-            <div className="mb-5">{header}</div>
-            <div className="flex flex-grow flex-col space-y-0.5 overflow-y-auto pb-3">
-              {content.map((item) => (
-                <ContentSelector key={item.id} content={item} useCollapsible={useCollapsible} />
-              ))}
-            </div>
+        <div className="@container flex h-full flex-col overflow-hidden">
+          {/* Header */}
+          <div className="mb-5 mt-4.5 px-3.5">{header}</div>
+
+          {/* Scrollable Content */}
+          <div className="flex flex-1 flex-col space-y-4.5 overflow-y-auto px-3.5 pb-3">
+            {content.map((item) => (
+              <ContentSelector key={item.id} content={item} useCollapsible={useCollapsible} />
+            ))}
           </div>
+
+          {/* Footer - always at bottom */}
           {(footer || useCollapsible) && (
-            <SidebarFooter useCollapsible={useCollapsible}>{footer}</SidebarFooter>
+            <div className="px-3.5">
+              <SidebarFooter useCollapsible={useCollapsible}>{footer}</SidebarFooter>
+            </div>
           )}
         </div>
       </SidebarContextProvider>
