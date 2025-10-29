4ae02540895753c0e281110b27f46ee2fca049c0
Nate Kelley <nate@buster.so>
2025-10-29 10:49:23 -0600

    scrollbar for sidebar content

--- apps/web/src/components/ui/sidebar/SidebarComponent.tsx
+++ apps/web/src/components/ui/sidebar/SidebarComponent.tsx
@@ -4,6 +4,7 @@ import { SidebarCollapsible } from './SidebarCollapsible';
 import { SidebarContextProvider } from './SidebarContext';
 import { SidebarFooter } from './SidebarFooter';
 import { SidebarItem } from './SidebarItem';
+import './sidebar-module.css';
 
 export const Sidebar: React.FC<SidebarProps> = React.memo(
   ({ header, content, footer, useCollapsible = true, onCollapseClick }) => {
@@ -14,10 +15,16 @@ export const Sidebar: React.FC<SidebarProps> = React.memo(
           <div className="mb-5 mt-4.5 px-3.5">{header}</div>
 
           {/* Scrollable Content */}
-          <div className="flex flex-1 flex-col space-y-4.5 overflow-y-auto px-3.5 pb-3">
-            {content.map((item) => (
-              <ContentSelector key={item.id} content={item} useCollapsible={useCollapsible} />
-            ))}
+          <div className="sidebar-scroll-area flex flex-1 flex-col overflow-y-auto scrollbar scrollbar-thin">
+            {/* Top scroll indicator - appears when scrolled down */}
+            <div className="sidebar-scroll-indicator-top" />
+            <div className="flex flex-col space-y-4.5 px-3.5 pb-3">
+              {content.map((item) => (
+                <ContentSelector key={item.id} content={item} useCollapsible={useCollapsible} />
+              ))}
+            </div>
+            {/* Bottom scroll indicator - appears when content is scrollable */}
+            <div className="sidebar-scroll-indicator-bottom" />
           </div>
 
           {/* Footer - always at bottom */}
